63090b8361bad8568f88e1319e503bdcaf9227c7
rajin572 <din.islam.rajin572@gmail.com>
2025-01-18 09:31:07 +0600

    auth

---
+++ src/utils/authOptions.js
@@ -0,0 +1,15 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
