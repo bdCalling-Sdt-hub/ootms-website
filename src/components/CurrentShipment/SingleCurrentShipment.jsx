ea4a2be5d0b2f9b737b88d93bb2e9fdf859e0794
mirza2018 <mirza.eee.4th@gmail.com>
2025-01-20 12:42:01 +0600

    add

--- src/components/CurrentShipment/SingleCurrentShipment.jsx
+++ src/components/CurrentShipment/SingleCurrentShipment.jsx
@@ -15,8 +15,6 @@ const SingleCurrentShipment = () => {
   const [open, setOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
-  console.log("data amar kahini kore", data?.data);
-
   const router = useRouter();
   return (
     <div className="py-20 bg-[#F3F3F3]">
@@ -47,39 +45,22 @@ const SingleCurrentShipment = () => {
                         <FaStar className="text-yellow-400" />
                       </p>
                       <p>4.5</p> */}
-                      <p>
-                        {
-                          data?.data?.attributes?.loadRequests[0]?.driver
-                            ?.fullName
-                        }
-                      </p>
+                      <p>{data?.data?.attributes[0]?.driver?.fullName}</p>
                     </div>
                   </div>
                   <div>
                     <p className="text-lg font-semibold">Driver Phone: </p>
-                    <p>
-                      {
-                        data?.data?.attributes?.loadRequests[0]?.driver
-                          ?.phoneNumber
-                      }
-                    </p>
+                    <p>{data?.data?.attributes[0]?.driver?.phoneNumber}</p>
                   </div>
                 </div>
                 <div className="md:flex justify-between">
                   <div>
                     <p className="text-lg font-semibold">Driver Email: </p>
-                    <p>
-                      {data?.data?.attributes?.loadRequests[0]?.driver?.email}
-                    </p>
+                    <p>{data?.data?.attributes[0]?.driver?.email}</p>
                   </div>
                   <div>
                     <p className="text-lg font-semibold">Driver Address: </p>
-                    <p>
-                      {
-                        data?.data?.attributes?.loadRequests[0]?.load
-                          ?.receivingAddress
-                      }
-                    </p>
+                    <p>{data?.data?.attributes[0]?.driver?.address}</p>
                   </div>
                 </div>
               </div>
@@ -93,20 +74,12 @@ const SingleCurrentShipment = () => {
                 <div className="flex justify-between">
                   <div>
                     <p className="text-lg font-semibold">Truck Number: </p>
-                    <p>
-                      {
-                        data?.data?.attributes?.loadRequests[0]?.truck
-                          ?.truckNumber
-                      }
-                    </p>
+                    <p>{data?.data?.attributes[0]?.truck?.truckNumber}</p>
                   </div>
                   <div>
                     <p className="text-lg font-semibold">Trailer Size: </p>
                     <p>
-                      {
-                        data?.data?.attributes?.loadRequests[0]?.truck
-                          ?.trailerSize
-                      }
+                      {data?.data?.attributes[0]?.truck?.trailerSize}
                       -foot trailer.
                     </p>
                   </div>
@@ -115,10 +88,7 @@ const SingleCurrentShipment = () => {
                   <div>
                     <p className="text-lg font-semibold">Pallet Spaces: </p>
                     <p>
-                      {
-                        data?.data?.attributes?.loadRequests[0]?.truck
-                          ?.palletSpace
-                      }
+                      {data?.data?.attributes[0]?.truck?.palletSpace}
                       pallets.
                     </p>
                   </div>
@@ -126,18 +96,14 @@ const SingleCurrentShipment = () => {
                     <p className="text-lg font-semibold">Availability: </p>
                     {/* <p>Fully Available.</p> */}
 
-                    {data?.data?.attributes?.loadRequests[0]
-                      ?.availablePalletSpace > 5 && (
+                    {data?.data?.attributes[0]?.availablePalletSpace > 5 && (
                       <p>The truck is fully available.</p>
                     )}
-                    {data?.data?.attributes?.loadRequests[0]
-                      ?.availablePalletSpace === 0 && (
+                    {data?.data?.attributes[0]?.availablePalletSpace === 0 && (
                       <p>The truck is fully loaded.</p>
                     )}
-                    {data?.data?.attributes?.loadRequests[0]
-                      ?.availablePalletSpace > 0 &&
-                      data?.data?.attributes?.loadRequests[0]
-                        ?.availablePalletSpace <= 5 && (
+                    {data?.data?.attributes[0]?.availablePalletSpace > 0 &&
+                      data?.data?.attributes[0]?.availablePalletSpace <= 5 && (
                         <p>The truck has low pallet space.</p>
                       )}
                   </div>
@@ -156,39 +122,22 @@ const SingleCurrentShipment = () => {
                 <p className="font-semibold">Reciver Name</p>
 
                 <div className="flex items-center">
-                  <span>
-                    {
-                      data?.data?.attributes?.loadRequests[0]?.load
-                        ?.receiverName
-                    }
-                  </span>
+                  <span>{data?.data?.attributes[0]?.load?.receiverName}</span>
                 </div>
               </div>
               <div>
                 <p className="font-semibold">Reciver Phone</p>
-                <p>
-                  {" "}
-                  {
-                    data?.data?.attributes?.loadRequests[0]?.load
-                      ?.receiverPhoneNumber
-                  }
-                </p>
+                <p> {data?.data?.attributes[0]?.load?.receiverPhoneNumber}</p>
               </div>
               <div>
                 <p className="font-semibold">Reciver Email</p>
-                <p>
-                  {" "}
-                  {
-                    data?.data?.attributes?.loadRequests[0]?.load?.receiverEmail
-                  }{" "}
-                </p>
+                <p> {data?.data?.attributes[0]?.load?.receiverEmail} </p>
               </div>
               <div>
                 <p className="font-semibold">Reciver Address</p>
-                {
-                  data?.data?.attributes?.loadRequests[0]?.load
-                    ?.receivingAddress
-                }
+                {data?.data?.attributes[0]?.load?.receivingAddress},{" "}
+                {data?.data?.attributes[0]?.load?.receiverCity},{" "}
+                {data?.data?.attributes[0]?.load?.receiverState}
               </div>
             </div>
           </div>
@@ -201,21 +150,18 @@ const SingleCurrentShipment = () => {
               <div className="flex flex-col gap-4">
                 <div>
                   <p className="text-lg font-semibold">Load Type: </p>
-                  <p>
-                    {data?.data?.attributes?.loadRequests[0]?.load?.loadType}
-                  </p>
+                  <p>{data?.data?.attributes[0]?.load?.loadType}</p>
                 </div>
                 <div>
                   <p className="text-lg font-semibold">Weight: </p>
-                  <p>{data?.data?.attributes?.loadRequests[0]?.load?.weight}</p>
+                  <p>{data?.data?.attributes[0]?.load?.weight}</p>
                 </div>
                 <div>
                   <p className="text-lg font-semibold">Pickup: </p>
                   <p>
-                    {
-                      data?.data?.attributes?.loadRequests[0]?.load
-                        ?.shippingAddress
-                    }
+                    {data?.data?.attributes[0]?.load?.shippingAddress},{" "}
+                    {data?.data?.attributes[0]?.load?.shippingCity},{" "}
+                    {data?.data?.attributes[0]?.load?.shippingState}
                   </p>
                 </div>
               </div>
@@ -223,16 +169,16 @@ const SingleCurrentShipment = () => {
                 <div>
                   <p className="text-lg font-semibold">Trailer Size: </p>
                   <p>
-                    {data?.data?.attributes?.loadRequests[0]?.load?.trailerSize}
+                    {data?.data?.attributes[0]?.load?.trailerSize}
                     -foot trailer—
-                    {data?.data?.attributes?.loadRequests[0]?.load?.palletSpace}
+                    {data?.data?.attributes[0]?.load?.palletSpace}
                     pallets.
                   </p>
                 </div>
                 <div>
                   <p className="text-lg font-semibold">HazMat: </p>
-                  {data?.data?.attributes?.loadRequests[0]?.load?.Hazmat
-                    ? data?.data?.attributes?.loadRequests[0]?.load?.Hazmat?.map(
+                  {data?.data?.attributes[0]?.load?.Hazmat
+                    ? data?.data?.attributes[0]?.load?.Hazmat?.map(
                         (value, i) => (
                           <span
                             className=" bg-[#2B4257]/20 me-2 rounded px-2 py-1"
@@ -247,10 +193,9 @@ const SingleCurrentShipment = () => {
                 <div>
                   <p className="text-lg font-semibold">Delivery: </p>
                   <p>
-                    {
-                      data?.data?.attributes?.loadRequests[0]?.load
-                        ?.receivingAddress
-                    }
+                    {data?.data?.attributes[0]?.load?.receivingAddress},{" "}
+                    {data?.data?.attributes[0]?.load?.receiverCity},{" "}
+                    {data?.data?.attributes[0]?.load?.receiverState}
                   </p>
                 </div>
               </div>
@@ -293,7 +238,7 @@ const SingleCurrentShipment = () => {
       {isOpen && (
         <ReAssign
           setIsOpen={setIsOpen}
-          loadId={data?.data?.attributes?.loadRequests[0]?.load?._id}
+          loadId={data?.data?.attributes[0]?.load?._id}
           id={params?.id}
         />
       )}
